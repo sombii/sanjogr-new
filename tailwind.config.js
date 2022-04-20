3ed911da9ed294361cf66b3c147ce59b284b855c
sombii <blackknightstd@gmail.com>
2022-04-20 13:31:27 +0545

    react and tailwindcss upgraded to 18 and 3 respectively

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,14 @@
 module.exports = {
-  // mode: "jit",
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {
-      fontFamily: {
-        workSans: "'Poppins', sans-serif",
-      },
-      fontsize: {
-        "2vw": "20vw"
-      },
+    content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+    theme: {
+        extend: {
+            fontFamily: {
+                workSans: "'Poppins', sans-serif",
+            },
+            fontsize: {
+                "2vw": "20vw"
+            },
+        },
     },
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+    plugins: [],
 }
